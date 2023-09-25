18a1af1f1e6dc017150bca122b955c7ecc14ec39
azizsigar <aazizsigar@gmail.com>
2023-09-25 15:16:34 +0200

    randomhex

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 
 function App() {
   // functions
+const [hex, setHex] = useState("#fffff")
+function randomHex(){
+  const randomHex = '#' + Math.floor(Math.random()*16777721).toString(16)
+  setHex(randomHex)
+}
 
   return (
     // htmls
-<div>
-  hi
+<div className='App' style={{backgroundColor:`${hex}`}}>
+  <div className='colorname'> colors</div>
+  <h1>{hex}</h1>
+<button onClick={randomHex}>random color</button>
+<button onClick={()=> navigator.clipboard.writeText(hex)}>copy color</button>
     </div>
   );
 }
