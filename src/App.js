a7c3e773628bd7ac1d1e1f8f3985d8dcd8b86582
azizsigar <aazizsigar@gmail.com>
2023-09-25 14:23:17 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -8,25 +8,7 @@ function App() {
   return (
     // htmls
 <div>
-  <Card
-  img='https://img.freepik.com/premium-photo/turkish-fast-food-chicken-shawarma-tortilla-with-mayonnaise-vegetables-isolated-white-background_124507-28563.jpg?w=2000'
-  title='title'
-  desc='description'
-  />
-  <Card
-  img='https://img.freepik.com/premium-photo/turkish-fast-food-chicken-shawarma-tortilla-with-mayonnaise-vegetables-isolated-white-background_124507-28563.jpg?w=2000'
-  title='title'
-  desc='description'
-  />
-  <Card
-  img='https://img.freepik.com/premium-photo/turkish-fast-food-chicken-shawarma-tortilla-with-mayonnaise-vegetables-isolated-white-background_124507-28563.jpg?w=2000'
-  title='title'
-  desc='description'
-  /><Card
-  img='https://img.freepik.com/premium-photo/turkish-fast-food-chicken-shawarma-tortilla-with-mayonnaise-vegetables-isolated-white-background_124507-28563.jpg?w=2000'
-  title='title'
-  desc='description'
-  />
+  hi
     </div>
   );
 }
