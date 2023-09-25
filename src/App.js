4083df8929beeb328fd83d1dc9c8f7880bb10b3f
azizsigar <aazizsigar@gmail.com>
2023-09-25 11:17:06 +0200

    random

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import './App.css';
 
 function App() {
   // functions
-
+const[minVal,setMinVal] =useState()
+const[maxVal,setMaxVal] =useState()
+const[randomNum,setRandomNum] =useState()
+const getRandomNum =()=>{
+  setRandomNum(Math.floor(Math.random()*(maxVal-minVal +1)+minVal))
+}
   return (
     // htmls
 <div>
-  hello
+  hi
+  <div>random num <span>{randomNum}</span></div>
+  <div>min</div>
+  <input type='number' value={minVal} onChange={e => setMinVal(+e.target.value)}></input>
+  <div>max</div>
+  <input type='number' value={maxVal} onChange={e => setMaxVal(+e.target.value)}></input>
+  <button onClick={getRandomNum}>get random num</button>
     </div>
   );
 }
