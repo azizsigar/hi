078f2cf37044c2851749b2d2d4cfc54d2ed30ac6
azizsigar <aazizsigar@gmail.com>
2023-09-25 12:09:06 +0200

    hi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
-import './App.css';
+import './App.css'
+import images from "./img";
+
 
 function App() {
   // functions
-
+const [seletcedImg, setSelectedImg] = useState(images[0])
   return (
     // htmls
 <div>
   hello
+  <div>
+<img src={seletcedImg} className='selected'></img>
+  </div>
+  <div>
+    <p>click and see bigger</p>
+    {images.map((img,index)=>(
+      <img key={index} src={img} alt='hi'
+      onClick={()=> setSelectedImg(img)}></img>
+))}
+  </div>
     </div>
   );
 }
---
+++ src/img.js
@@ -0,0 +1,9 @@
+export default[
+    "https://upload.wikimedia.org/wikipedia/commons/b/bc/Tantuni_sokak_yeme%C4%9Fi.jpg",
+    "https://imgrosetta.mynet.com.tr/file/11990709/11990709-728xauto.jpg",
+    "https://media-cdn.tripadvisor.com/media/photo-s/0e/ac/1d/e7/ekmek-tantuni.jpg",
+    "https://i.ytimg.com/vi/ZsuDMqjEcCQ/maxresdefault.jpg",
+    "https://cdn.getiryemek.com/products/1552376081831_500x375.png",
+    "https://images.squarespace-cdn.com/content/v1/5a2915cebff20052a88638f2/e8386f53-52e8-4f4f-85ef-3ad928d4309b/Vegan+Lahmacun+for+blog+title.jpeg?format=1500w",
+    "https://previews.123rf.com/images/alpaksoy/alpaksoy1712/alpaksoy171201078/92313401-kumpir-turkse-gebakken-aardappel-met-kaas-ma%C3%AFs-worst-ketchup-en-mayonaise-fast-food.jpg"
+]
